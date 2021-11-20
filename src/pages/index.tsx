94669e1a4edd5f43ff5972bc38bdc32eea68e721
Fiali1 <fiali1@hotmail.com>
2021-11-20 20:34:30 -0300

    etapa 2 - login e dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
-export default function Home() {
+import { Flex, Button, FormLabel, FormControl, Stack } from '@chakra-ui/react';
+
+import { Input } from '../components/Form/Input'; 
+
+export default function SignIn() {
   return (
-    <h1>Hello World</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        flexDir="column"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Password" />
+        </Stack>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink"
+        >
+          Login
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
